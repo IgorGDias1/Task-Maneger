fd92f351c3cdd0a44af541161f481422b4c728fa
Igor Galvão Dias <105743982+IgorGDias1@users.noreply.github.com>
2025-07-29 15:39:10 -0300

    Implementar visualização de detalhes da tarefa via parâmetros de URL na rota /task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -14,6 +24,7 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
             {task.title}
           </button>
           <button
+            onClick={() => onSeeDetailsClick(task)}
             className={`text-white p-2 rounded-md ${
               task.isCompleted ? "bg-green-500" : "bg-slate-400"
             }`}
