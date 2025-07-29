fbe5cac02450c5b9ba4be5295f6681b3a5abdfde
Igor Galvão Dias <105743982+IgorGDias1@users.noreply.github.com>
2025-07-29 14:42:42 -0300

    criar componente AddTask com inputs para título e descrição

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,36 @@
-function AddTask(){
-    <h1>AddTask</h1>
+import { useState } from "react";
+
+function AddTask({ onAddTaskClick }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      <input
+        type="text"
+        placeholder="Adicionar tarefa"
+        className="border border-slate-500 p-2 rounded-md w-full"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Descrição da tarefa"
+        className="border border-slate-500 p-2 rounded-md w-full"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          onAddTaskClick(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 p-2 rounded-md w-full text-white"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
-export default AddTask
+export default AddTask;
